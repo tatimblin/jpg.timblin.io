4d6c1b309a1f34bc0662e2ed2bdd959f985ca648
Tristan Timblin <ttimblin@Tristans-MacBook-Pro.local>
2019-08-29 07:11:37 -0400

    mobile browser fix

--- plugins/next-page.client.js
+++ plugins/next-page.client.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 Vue.prototype.$triggerNextPage = (callback) => {
 	const grace = 50;
 	window.onscroll = () => {
-		if (window.scrollY > 1) {
+		if (window.scrollY > grace) {
 			if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - grace) {
 				callback();
 			}
