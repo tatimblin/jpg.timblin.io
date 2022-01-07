beaf40db97e7db403a0aa44fa804db02b06ee16e
tatimblin@gmail.com <tatimblin@gmail.com>
2022-01-06 22:45:06 -0500

    add static index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,36 @@
 <template>
+  <main class="Main">
+    <div class="Main-content">
+      <h1 class="Main-hero">
+        {{ hero }}
+      </h1>
+      <nuxt-link to="start">Start</nuxt-link>
+    </div>
+  </main>
 </template>
 
 <script>
 import { createClient } from '~/plugins/contentful.js'
-const client = createClient()
+const client = createClient();
 
 export default {
   layout: 'default',
-  mounted() {
-    const postTypeID = this.$config.postTypeID;
-    const slug = client.getEntries({
-      'content_type': postTypeID,
-      'order': '-fields.date',
-      'limit': 1,
+  asyncData () {
+    return client.getEntries({
+      'content_type': 'homepage',
     })
-    .then(entries => {
-      this.$router.push(entries.items[0].fields.slug)
-    })
+      .then((entries) => entries.items[0].fields)
+      .catch(console.error);
   },
 };
 </script>
+
+<style lang="scss">
+.Main
+{
+	&-hero
+	{
+    @include lead;
+	}
+}
+</style>
---
+++ pages/start.vue
@@ -0,0 +1,20 @@
+<template>
+</template>
+
+<script>
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient();
+
+export default {
+  layout: 'default',
+  mounted() {
+    const postTypeID = this.$config.postTypeID;
+    client.getEntries({
+      'content_type': postTypeID,
+      'order': '-fields.date',
+      'limit': 1,
+    })
+      .then(entries => this.$router.replace(entries.items[0].fields.slug));
+  },
+};
+</script>
