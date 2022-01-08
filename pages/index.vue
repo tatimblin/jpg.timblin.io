0dfb2428d22fac831b2afcbdf48ab6cbe84dfa5b
tatimblin@gmail.com <tatimblin@gmail.com>
2022-01-07 21:37:43 -0500

    add post list to index

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,13 @@
         {{ hero }}
       </h1>
       <nuxt-link to="start">Start</nuxt-link>
+      <ul class="Main-list" v-if="items.length">
+        <li class="Main-item" v-for="item in items" :key="item.id">
+          <nuxt-link :to="item.fields.slug">
+            {{item.fields.title}}
+          </nuxt-link>
+        </li>
+      </ul>
     </div>
   </main>
 </template>
@@ -15,11 +22,19 @@ const client = createClient();
 
 export default {
   layout: 'default',
-  asyncData () {
+  asyncData ({ $config: { postTypeID }}) {
     return client.getEntries({
       'content_type': 'homepage',
     })
-      .then((entries) => entries.items[0].fields)
+      .then((entries) => {
+        return client.getEntries({
+          'content_type': postTypeID,
+        })
+          .then(({ items }) => {
+            return {...entries.items[0].fields, items};
+          })
+          .catch(console.error);
+      })
       .catch(console.error);
   },
 };
@@ -28,9 +43,34 @@ export default {
 <style lang="scss">
 .Main
 {
+  &-content
+  {
+    margin: 80px auto;
+
+    @include query(small)
+    {
+      width: map-get($breakpoints, small);
+    }
+
+    @include query(large)
+    {
+      width: map-get($breakpoints, large);
+    }
+  }
+
 	&-hero
 	{
     @include lead;
 	}
+
+  &-list
+  {
+    padding: 32px 0;
+  }
+
+  &-item
+  {
+    @include sub;
+  }
 }
 </style>
